b146545494c19b89bc0e1d147042ed8a8be4cc3b
jadg13 <jadg13@hotmail.com>
2024-02-03 09:56:17 -0600

    Ejemplo de uso de funciones

---
+++ ejemplo1.py
@@ -0,0 +1,6 @@
+import os
+os.system("cls || clear")
+def getFullName(firstName = "Jose", lastName="Duran"):
+    return firstName + ' ' + lastName
+
+print(f"Tu nombre completo es {getFullName()}")
