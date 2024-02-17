39fef37ed7bab9e84cbe7afd761dd81b197554af
jadg13 <jadg13@hotmail.com>
2024-02-17 09:08:01 -0600

    [update] agregue otro codigo

--- Sesion5/ejemplo3.py
+++ Sesion5/ejemplo3.py
@@ -1,1 +1,12 @@
 #listas
+lista = [1, 2, 3, "Manzana", 15.6, True]
+print(lista)
+
+#imprimir el primer elemento de la lista
+print(lista[0])
+#tamaño de la lista
+tam = len(lista)
+print(f"el tamaño es {tam}")
+#imprimir el ultimo valor
+print(lista[tam-1])
+
